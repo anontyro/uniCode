9f86362b02a532172172bb607d0046f5bcf6f9b7
Alex <Alex@Alex-PC>
2016-06-17 01:10:59 +0800

    Jun 17 start of the course first code

---
+++ src/week1/java/GravityCalculator.java
@@ -0,0 +1,30 @@
+
+package week1.java;
+
+/**
+ *
+ * @author Alex
+ */
+
+import java.io.*;
+
+public class GravityCalculator {    
+    
+    public static void main(String[]args){
+        
+        double gravity = -9.81;
+        double initialVelocity = 0.0;
+        double fallingTime = 10.0;
+        double initalPosition = 1000.0;
+        double finalPosition = 0.0;
+
+        finalPosition = ((0.5 * gravity * (Math.pow(fallingTime,2))) 
+                + (initalPosition*fallingTime) + initalPosition) /fallingTime ;
+         
+        
+        
+        System.out.println("The object's position after " + fallingTime +
+                "seconds is " + finalPosition + "m.");
+    }
+    
+}
