b9a30ceed9193a784cef438f90c7be8ae28e9282
Alex <Alex@Alex-PC>
2016-07-03 01:36:55 +0800

    July 03 updated IceCreamSold to have toString and toString(String icecream) methods to allow for easy report generation, all JavaDoc in place and extensive commenting has been put in place as this will be assessed by some people with limited Java knowledge. Overall it is finish just need to test with the offical icecream.dat file but there is a lot of room for growth and even database intergration to store the data and call it

--- src/week3/java/IceCreamSold.java
+++ src/week3/java/IceCreamSold.java
@@ -1,16 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package week3.java;
 
-import java.io.*;
+import java.io.*; //class import for IO usage
+
 
 
 /**
- *
- * @author Alex
+ * A simple class that reads from a file the number of ice creams sold, 
+ * ice creams must be separated with a newline. This class uses its own file reading
+ * method to find the data from the file
+ * 
  */
 public class IceCreamSold {
     
@@ -18,9 +16,14 @@ public class IceCreamSold {
     private String FavIceCream = "";
     private String[]totalSales;
     
+    /**
+    * Constructor which takes the filename to be used for the data to be read
+    * to the IceCreamSold object. The constructor also catches IOExceptions
+    * that may occur from not finding the files
+    */
     IceCreamSold(String filename){
         try{
-            totalSales = ReadFile("icecream.dat");
+            totalSales = ReadFile(filename);
             iceCreamSales = totalSales.length;
         }
         catch(IOException e){
@@ -29,6 +32,17 @@ public class IceCreamSold {
         }
     }
     
+    /**
+     * The main Read class that throws an IOException if there is any issue with
+     * the chosen file. This method is called in the creation of the IceCreamSold
+     * object however it is also static so can be called from IceCreamSold.ReadFile(filename)
+     * @param filename the filename you wish to locate to import into the object
+     * it will take a string and needs a suitable suffix
+     * @return returns an Array of strings which can be used to generate a rundown
+     * of what the file contains
+     * @throws IOException exception thrown in the finally statement which cannot
+     * easily be handled within the method
+     */
     public static String[] ReadFile(String filename) throws IOException{
         File inFile = new File(filename);
         BufferedReader bin = null;
@@ -46,8 +60,7 @@ public class IceCreamSold {
             sales = fileReading.split("\n");
             
             return sales;
-
-            
+   
         }
         catch(IOException e){
             System.err.println(e.getMessage());
@@ -58,53 +71,69 @@ public class IceCreamSold {
        return sales;
     }
     
+    /**
+     * Get method to return a String of the sales that the file contains, each
+     * element is separated by a new line
+     * @return 
+     */
     public String getSales(){
         String sales = "";
         
         for(String x: totalSales){
             sales += x + "\n";
         }
-        
         return sales;
     }
     
-    public String numberOfSales(){
-        String total = "";
+    /**
+     * 
+     * @return 
+     */
+    public int numberOfSales(){
         
-        total = "The total number of sales are: " + iceCreamSales;
-        
-        return total;
+        return iceCreamSales;
     }
     
-    public String TypeOfIceCreamSold(String icecream){
+    /**
+     * 
+     * @param icecream
+     * @return 
+     */
+    public int TypeOfIceCreamSold(String icecream){
         String output ="";
         int numberSold = checkHowMany(icecream);
         
-        if(numberSold ==0){
-            return output ="Sorry none of those where sold!";
-        }
-        
-        output = "The number of " +icecream +" Sold was: " 
-                +numberSold;
-        
-        return output;
+        return numberSold;
     }
     
+    /**
+     * Returns an int of the number of sales made, it does this by reading how
+     * many entries are in the String Array totalsales
+     * @return 
+     */
     public int getTotalSales(){
         return iceCreamSales;
     }
     
-    public String percentIceSale(String icecream){
-        String output = "";
+    /**
+     * Simple method that takes a String of what value to search for. This method
+     * calls a private method that checks how many of this icecream have been sold
+     * it runs through the String array and returns an int value
+     * @param icecream the string name of the type of ice cream
+     * @return float for what percentage of this ice cream has been sold
+     */
+    public float percentIceSale(String icecream){
         int sales = checkHowMany(icecream);
         int totalSales = getTotalSales();
-        float percent =(float)((sales *100) / totalSales);
         
-        output = "The percentage of " +icecream +" sold was: " +percent +"%";
+        if(sales ==0){
+            return 0;
+        }
         
-        return output;
+        return (float)((sales *100) / totalSales);
     }
     
+    //private method used to check how many icecream of a type exisit in the file
     private int checkHowMany(String icecream){
         int numberSold = 0;
         
@@ -114,17 +143,60 @@ public class IceCreamSold {
             }
         }
         
-        return numberSold;
+        return numberSold; //returns an int value of how many
+    }
+    
+    /**
+     * generates a report of the output in a sensible manner which returns the
+     * total sales with the sales reports printed on seperate lines and then
+     * the total number of sales
+     * @return 
+     */
+    @Override
+    public String toString(){
+        String output = "";
+        output += "The total sales are: \n" + getSales() +"\n"
+                + "The total number of sales are: " + numberOfSales() +"\n"
+                ;
+        
+        
+        return output;
+    }
+    
+    /**
+     * This overloaded toString method takes the ice cream you want to report on
+     * as a parameter. It will print out the total sales, total number of sales
+     * the number of this ice cream sold and the percentage of this ice cream
+     * sold
+     * @param icecream
+     * @return 
+     */
+    public String toString(String icecream){
+        String output ="";
+        
+        output += "The total sales are: \n" + getSales() +"\n"
+                + "The total number of sales are: " +numberOfSales() + "\n"
+                + "The number of " + icecream + " sold is: " + TypeOfIceCreamSold(icecream) + "\n"
+                +"The percentage of " + icecream + " sold is: " + percentIceSale(icecream) +"%";
+        
+        return output;
     }
     
     
+    //main test class to ensure everything is working and run the above class
     public static void main(String[]args){
-        IceCreamSold ic = new IceCreamSold("icecream.dat");
+        /*
+        creates a new IceCreamSold object named "ic" which loads the data
+        from the file at the directory path/icecream.dat 
+        can be changed using .//src//package//name//icecream.dat
+        */
+        IceCreamSold ic = new IceCreamSold("./icecream.dat");
         
-        System.out.println(ic.getSales());
-        System.out.println(ic.numberOfSales());
-        System.out.println(ic.TypeOfIceCreamSold("Strawberry"));
-        System.out.println(ic.percentIceSale("Strawberry"));
+        /*
+        executing the toString parameter method to take the Strawberry string
+        and find all the information about this item
+        */
+        System.out.println(ic.toString("Strawberry"));
     }
 }
 
