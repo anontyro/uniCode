d495db16fcb397232a4b899f62a77247ae3080a3
Alex <Alex@Alex-PC>
2016-07-29 00:36:34 +0800

    July 29 - Finished and annotated all of the week7 it is now fully working and has been commented along with JavaDoc comments added to make the whole thing easy to read!

--- src/week7/java/Test.java
+++ src/week7/java/Test.java
@@ -10,10 +10,37 @@ package week7.java;
  * @author Alex
  */
 public class Test {
-    
-    public static void main(String[]args){
-        SquareMatrix sqMatrix = new SquareMatrix();
-        System.out.println(sqMatrix.toString());
-        System.out.println(sqMatrix.SecondDiagonal());
+
+    public static void main(String[] args) {
+        
+        //create first object
+        SquareMatrix sqMx = new SquareMatrix(3, 3);
+        int[] row = {10, 12, 11};
+        int[] row1 = {9, 8, 31};
+        int[] row2 = {2, 16, 24};
+        sqMx.addRow(0, row);
+        sqMx.addRow(1, row1);
+        sqMx.addRow(2, row2);
+
+        System.out.println(sqMx.toString());
+        System.out.println("Second Diagonal: " +sqMx.SecondDiagonal());
+        
+        //create second object
+        SquareMatrix sqMx2 = new SquareMatrix(4, 4);
+        int[] rowX = {41, 19, 1, 8};
+        int[] rowY = {10, 6, 44, 65};
+        int[] rowZ = {8, 21, 11, 71};
+        int[] rowW = {7, 5, 21, 10};
+
+        sqMx2.addRow(0, rowX);
+        sqMx2.addRow(1, rowY);
+        sqMx2.addRow(2, rowZ);
+        sqMx2.addRow(3, rowW);
+
+        System.out.println(sqMx2.toString());
+        System.out.println("Second Diagonal: " + sqMx2.SecondDiagonal());
+        
+        //create thrid object
+        SquareMatrix sqMx3 = new SquareMatrix(4,6);
     }
 }
